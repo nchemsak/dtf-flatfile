86f39bfe4e1bd0134e5db2e1bca67f53780605bd
Nicholas Chemsak <nchemsak@gmail.com>
2018-01-11 11:02:48 -0600

    added scroll to top button html/js/css

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,3 +4,31 @@ function add(a, b) {
     return a + b;
 }
 console.log("add(5, 12): ", add(5, 12));
+
+///////////////////////////// scroll to top button ////////////////////////////
+// When the user scrolls down 20px from the top of the document, show the button
+// button HTML is located on the FOOTER template.
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    var myB = document.getElementById("myBtn");
+    myB.style.display = "none";
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        if (myB) {
+           myB.style.display= "block";
+        }
+    } else {
+        if (myB) {
+            myB.style.display = "none";
+        }
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+     $('html, body').animate({ scrollTop: 0 }, 'easeInOutExpo', function () {
+    });
+
+}
